8785531f43333cd6befea087de87e4f63523f21c
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-11 19:00:26 +0500

    created product categories for models

--- backend/models.py
+++ backend/models.py
@@ -11,3 +11,10 @@ class NavSlide(models.Model):
 
     def __str__(self):
         return self.name
+
+class ProductCategories(models.Model):
+    class Meta:
+        verbose_name = 'Product Category'
+        verbose_name_plural = 'Product Categories'
+    
+    name = models.CharField(max_length=20)
