bee7edc460f9580d794c72170b966157d19fdae9
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-12 19:07:30 +0500

    some changes

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from .views import *
 
+app_name='e-shop'
+
 urlpatterns = [
-    path('', Home.as_view(), name="Home"),
-    path('shop/', Shop.as_view(), name="Shop"),
+    path('', Home.as_view(), name="home"),
+    path('shop/', Shop.as_view(), name="shop"),
+    path('detail/', ProductDetail.as_view(), name="product-detail"),
 ]
--- backend/views.py
+++ backend/views.py
@@ -32,3 +32,17 @@ class Shop(ListView):
         context['slides'] = NavSlide.objects.all()
         context['clotheType'] = ClotheTypeCategory.objects.all()
         return context
+
+class ProductDetail(TemplateView):
+    template_name = 'detail.html'
+    context_object_name = 'slides'
+
+    def get_queryset(self):
+        return NavSlide.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        # here we can add so many context using that way
+        context['slides'] = NavSlide.objects.all()
+        context['clotheType'] = ClotheTypeCategory.objects.all()
+        return context
