6a619f89362113ce5fbbbf1870cd50cf9f2d9a40
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-12 16:07:50 +0500

    connected clothe type categories to frontend

--- backend/models.py
+++ backend/models.py
@@ -19,9 +19,15 @@ class ProductCategories(models.Model):
     
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 class ClotheTypeCategory(models.Model):
     class Meta:
         verbose_name = 'Clothe type category'
         verbose_name_plural = 'Clothe type categories'
 
     name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
--- backend/views.py
+++ backend/views.py
@@ -5,4 +5,13 @@ from django.views.generic import TemplateView, ListView
 class Home(ListView):
     template_name = 'index.html'
     context_object_name = 'slides'
-    queryset = NavSlide.objects.all()
+
+    def get_queryset(self):
+        return NavSlide.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        # here we can add so many context using that way
+        context['slides'] = NavSlide.objects.all()
+        context['clotheType'] = ClotheTypeCategory.objects.all()
+        return context
