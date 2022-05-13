cb6f85911bafb30ca0b5e5cb6d4c3849575b1da9
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-13 17:23:36 +0500

    some changes

--- e_shopper/settings.py
+++ e_shopper/settings.py
@@ -104,13 +104,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static/static/",
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_URL = 'static/images/'
