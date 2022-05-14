2cf5a80a8a0f28788a48d3d0cbb237b98a71118f
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-14 12:58:04 +0500

    some changes in products and their details

--- backend/views.py
+++ backend/views.py
@@ -19,13 +19,32 @@ class Home(ListView):
 
 class Shop(ListView):
     template_name = 'shop.html'
-    queryset = ClotheTypeCategory.objects.all()
     context_object_name = 'clotheType'
+
+    def get_queryset(self):
+        return ClotheTypeCategory.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(Shop, self).get_context_data(**kwargs)
+        # here we can add so many context using that way
+        context['clotheType'] = ClotheTypeCategory.objects.all()
+        context['products'] = Products.objects.all()
+        return context
 
 class ProductDetail(DetailView):
     template_name = 'detail.html'
     queryset = Products.objects.all()
     context_object_name = 'product'
+
+    def get_queryset(self):
+        return Products.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        # here we can add so many context using that way
+        context['clotheType'] = ClotheTypeCategory.objects.all()
+        context['products'] = Products.objects.all()
+        return context
 
 class Contact(ListView):
     template_name = 'contact.html'
